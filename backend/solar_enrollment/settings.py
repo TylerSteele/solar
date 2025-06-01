a18814884ab3f88001ce36b65db508ed9ce07b83
SteeleTyler <tylermsteele@outlook.com>
2025-06-01 16:43:15 -0400

    Update Database connect config

--- backend/solar_enrollment/settings.py
+++ backend/solar_enrollment/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = 'solar_enrollment.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 # }DATABASES = {
 #     'default': {
